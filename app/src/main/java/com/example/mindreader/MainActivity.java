8c636ab66b37ad647e65ef141a4f0f702acbbb35
Dixitvara <varadixit@gmail.com>
2024-07-09 18:29:28 +0530

    change made in last activity UI

--- app/src/main/java/com/example/mindreader/MainActivity.java
+++ app/src/main/java/com/example/mindreader/MainActivity.java
@@ -4,20 +4,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    AlertDialog.Builder builder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.startBtn);
+        builder = new AlertDialog.Builder(this);
+
         button.setOnClickListener(view -> goToNextActivity());
     }
 
     public void onBackPressed() {
-        finishAffinity();
+        builder.setTitle("Exit app?");
+        builder.setMessage("Do you really want to close the app?").
+                setCancelable(false).
+                setPositiveButton("Yes",(dialogInterface, i) -> {
+                    finishAffinity();
+                }).
+                setNegativeButton("Cancel",(dialogInterface, i) -> {
+                    dialogInterface.cancel();
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
     public void goToNextActivity() {
